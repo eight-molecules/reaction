1930a4005472f68b042342aa928dd829e6ce0513
Grant Elliott <gelliott181@gmail.com>
2020-09-01 23:47:31 -0400

    toPromise method to allow easy conversion to promises.
    Add gitignore
    
    Recommitted with gitignore.

--- src/Observable.ts
+++ src/Observable.ts
@@ -4,6 +4,18 @@ import { Subscription } from "./Subscription";
 export class Observable<T> {
   constructor(private onSubscribe: (observer: Observer<T>) => Subscription | void = ({ complete = () => {} }) => complete()) { }
 
+  toPromise(): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      const { unsubscribe } = this.subscribe({
+        next: (value: any) => {
+          setTimeout(() => unsubscribe(), 0);
+          resolve(value);
+        },
+        error: (err: Error) => reject(err)
+      });
+    });
+  }
+
   subscribe(observer: Observer<T>): Subscription {
     const subscription = this.onSubscribe(observer);
     return subscription || { unsubscribe(): void { } };
